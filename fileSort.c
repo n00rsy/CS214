d43e3c3351224c6611ed85820aaab7802e2d1b73
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2020-02-13 18:48:33 -0500

    define types

--- fileSort.c
+++ fileSort.c
@@ -3,23 +3,51 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define INT_MAX 2147483647
+
+typedef union token_t {
+   char* str;
+   size_t len_str;
+} token; 
+
+typedef struct ArrayList_t{
+   int is_num;
+   size_t size;
+   size_t len;
+   token* things
+} array;
+/*
+ make a method to declare a new arraylist
+ make another method for add ( handle overlfow)
+
+*/
+void add(array a, token t);
+
 int main (int argc, char * argv[]){
+   if(argc!=3){
+      //throw error, incorrect arguements
+      printf("Incorrect arguements\n");
+      return 1;
+   }
+   int file = open(argv[2], O_RDONLY);
+   if(file==-1){
+      //throw error, file doesnt exist
+      printf("File does not exist\n");
+      return 1;
+   }
 
-  if(argc!=3){
-    //throw error, incorrect arguements
-    printf("Incorrect arguements\n");
-    return 1;
-  }
-  int file = open(argv[2], O_RDONLY);
-  if(file==-1){
-    //throw error, file doesnt exist
-    printf("File does not exist\n");
-    return 1;
-  }
-
-  char *c = (char *) calloc(1000000000, sizeof(char));
-  while(read(file,c,1000000000)>0){
-    printf("%s\n", c);
-    printf("AAAAAA\n");
-  }
+   char *c = (char *) calloc(INT_MAX, sizeof(char));
+   while(read(file,c,INT_MAX)>0){
+      printf("%s\n", c);
+      printf("AAAAAA\n");
+   }
 }
+
+/*
+
+   1) 2 pointers until comma
+   2) count len of token
+   3) allocate memory for token
+   4) add to data structure
+
+*/
