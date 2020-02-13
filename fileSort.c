22b9751ac6b409f26dbb375ad2b19fa599b10ed9
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2020-02-13 17:53:24 -0500

    add INT_MAX

--- fileSort.c
+++ fileSort.c
@@ -3,9 +3,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define INT_MAX 2147483647
+
+
 int main (int argc, char * argv[]){
 
-  if(argc!=3){
+  if(argc != 3){
     //throw error, incorrect arguements
     printf("Incorrect arguements\n");
     return 1;
@@ -17,8 +20,8 @@ int main (int argc, char * argv[]){
     return 1;
   }
 
-  char *c = (char *) calloc(1000000000, sizeof(char));
-  while(read(file,c,1000000000)>0){
+  char *c = (char *) calloc(INT_MAX, sizeof(char));
+  while(read(file,c,INT_MAX)>0){
     printf("%s\n", c);
     printf("AAAAAA\n");
   }
